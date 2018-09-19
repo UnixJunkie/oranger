b038af49b2837e93a7b94908fa29bf55faf21365
Francois BERENGER <unixjunkie@sdf.org>
2018-09-19 12:01:13 +0900

    Test: classify is tested

--- src/test.ml
+++ src/test.ml
@@ -1,9 +1,20 @@
+open Printf
+
+module L = BatList
 
 let main () =
   Log.(set_log_level DEBUG);
   Log.color_on ();
   assert(
-    Oranger.RF.(train ~debug:true ~nprocs:4 Classification 500
-                  "data/iris.txt" "Species" "ranger_model.rf"))
+    Oranger.RF.(train ~debug:true Classification 500
+                  "data/iris.txt" "Species" "ranger_model.rf"));
+  let preds =
+    Oranger.RF.classify ~debug:true 500 "data/iris.txt" "ranger_model.rf" in
+  match preds with
+  | None -> assert(false)
+  | Some xs ->
+    L.iter (fun (mean, stddev) ->
+        printf "%f %f\n" mean stddev
+      ) xs
 
 let () = main ()
