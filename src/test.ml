b53d69897fa9664099ae545611e320d56b6400c6
Francois BERENGER <unixjunkie@sdf.org>
2018-09-19 12:44:30 +0900

    Test: output AUC on test set

--- src/test.ml
+++ src/test.ml
@@ -2,6 +2,15 @@ open Printf
 
 module L = BatList
 
+module Score_label = struct
+  type t = bool * float (* (label, pred_score) *)
+  let get_label (l, _) = l
+  let get_score (_, s) = s
+end
+
+module ROC = Cpm.MakeROC.Make(Score_label)
+
+
 let main () =
   Log.(set_log_level DEBUG);
   Log.color_on ();
@@ -14,9 +23,15 @@ let main () =
     Oranger.RF.classify ~debug:true nb_trees "data/test.txt" "ranger_model.rf" in
   match preds with
   | None -> assert(false)
-  | Some xs ->
-    L.iter (fun (mean, stddev) ->
-        printf "%f %f\n" mean stddev
-      ) xs
+  | Some score_stddevs ->
+    (* L.iter (fun (mean, stddev) ->
+     *     printf "%f %f\n" mean stddev
+     *   ) xs *)
+    let labels = [true; true; true; true; true;
+                  false; false; false; false; false] in
+    let scores = L.map fst score_stddevs in
+    let score_labels = L.combine labels scores in
+    let auc = ROC.auc score_labels in
+    printf "AUC: %.3f\n" auc
 
 let () = main ()
