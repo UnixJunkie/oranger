ef4be289b712f9d2089b729ea035599db4817aa3
Francois BERENGER <unixjunkie@sdf.org>
2018-09-19 12:00:51 +0900

    Utls: smaller

--- src/utls.ml
+++ src/utls.ml
@@ -8,35 +8,12 @@ let with_in_file fn f =
   close_in input;
   res
 
-let with_out_file fn f =
-  let output = open_out_bin fn in
-  let res = f output in
-  close_out output;
-  res
-
-let ignore_fst _fst snd =
-  snd
-
-let fold_on_lines_of_file fn f acc =
-  with_in_file fn (fun input ->
-      let acc' = ref acc in
-      try
-        while true do
-          acc' := f !acc' (input_line input)
-        done;
-        assert(false)
-      with End_of_file -> !acc'
-    )
-
-let float_list_of_file fn =
-  let res =
-    fold_on_lines_of_file fn (fun acc line ->
-        let pred =
-          try Scanf.sscanf line "%f" (fun x -> x)
-          with Scanf.Scan_failure msg ->
-            (* percolate a NaN rather than crashing *)
-            (Log.error "%s: %s" msg line;
-             nan) in
-        pred :: acc
-      ) [] in
-  L.rev res
+(* population standard deviation *)
+let stddev (l: float list): float =
+  let n, sx, sx2 =
+    List.fold_left (fun (n, sx, sx2) x ->
+        (n +. 1., sx +. x, sx2 +. (x *.x))
+      ) (0., 0., 0.) l
+  in
+  sqrt ((sx2 -. (sx *. sx) /. n) /. n)
+(* stddev [2.; 4.; 4.; 4.; 5.; 5.; 7.; 9.] = 2.0 *)
