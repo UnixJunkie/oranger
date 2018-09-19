4555d089d763a9020f6c5acdc64b054e11644e0b
Francois BERENGER <unixjunkie@sdf.org>
2018-09-19 12:01:00 +0900

    RF: added classify

--- src/RF.ml
+++ src/RF.ml
@@ -1,6 +1,9 @@
 
 open Printf
 
+module A = BatArray
+module L = BatList
+
 type filename = string
 
 type mode = Regression
@@ -40,3 +43,59 @@ let train
       (Sys.rename (model_out_fn ^ ".forest") model_out_fn;
        true)
     | _ -> false
+
+let read_raw_class_predictions nb_trees fn =
+  let status, predicted_classes_str =
+    (* keep only integer lines *)
+    BatUnix.run_and_read
+      (sprintf "awk '/^[0-9]+$/{print $0}' %s" fn) in
+  assert(status = WEXITED 0);
+  let pred_strings =
+    BatString.nsplit ~by:"\n" (BatString.strip predicted_classes_str) in
+  let nb_preds = L.length pred_strings in
+  Log.debug "nb integer preds: %d" (L.length pred_strings);
+  let pred_classes = L.map float_of_string pred_strings in
+  let nb_samples = nb_preds / nb_trees in
+  Log.debug "nb samples: %d" nb_samples;
+  let preds = A.of_list pred_classes in
+  let res = ref [] in
+  for samp_i = 0 to nb_samples - 1 do
+    (* gather class predictions for this sample *)
+    let curr_preds = ref [] in
+    for tree_j = 0 to nb_trees - 1 do
+      let offset = (tree_j * nb_samples) + samp_i in
+      curr_preds := preds.(offset) :: !curr_preds
+    done;
+    (* compute mean and stddev *)
+    let avg = L.favg !curr_preds in
+    let std = Utls.stddev !curr_preds in
+    res := (avg, std) :: !res
+  done;
+  L.rev !res
+
+let classify
+    ?debug:(debug = false)
+    ?nprocs:(nprocs = 1)
+    (nb_trees: int)
+    (data_fn: filename)
+    (model_fn: filename): (float * float) list option =
+  let predictions_fn = Filename.temp_file "oranger_" "" in
+  let cmd =
+    sprintf
+      "ml_rf_ranger %s \
+       --file %s --predict %s --nthreads %d --outprefix %s --predall"
+      (if debug then "--verbose" else "")
+      data_fn
+      model_fn
+      nprocs
+      predictions_fn in
+  Log.debug "cmd: %s" cmd;
+  let status, log = BatUnix.run_and_read cmd in
+  Log.info "%s" log;
+  match status with
+  | WEXITED 0 ->
+    begin
+      let raw_preds_fn = predictions_fn ^ ".prediction" in
+      Some (read_raw_class_predictions nb_trees raw_preds_fn)
+    end
+  | _ -> None
