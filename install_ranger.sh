6dc0d1cc00fb3239536d1f8793c3a0313b381665
Francois Berenger <unixjunkie@sdf.org>
2019-11-25 17:18:51 +0900

    hardened install_ranger.sh script

--- install_ranger.sh
+++ install_ranger.sh
@@ -9,11 +9,13 @@ set -e
 #git clone --depth 1 https://github.com/imbs-hl/ranger.git
 
 # released-0.9.11
-wget https://github.com/imbs-hl/ranger/archive/0.9.11.tar.gz
+if [ ! -f 0.9.11.tar.gz ]; then
+    wget https://github.com/imbs-hl/ranger/archive/0.9.11.tar.gz
+fi
 tar xzf 0.9.11.tar.gz
 
 cd ranger-0.9.11/cpp_version
-mkdir build
+mkdir -p build
 cd build
 cmake ../
 make -j `getconf _NPROCESSORS_ONLN`
